42a182f7e072049fb9cd615909901de10aaa3b2b
Brian Hudson <reallybrianhudson@gmail.com>
2022-01-14 20:02:29 -0500

    Working

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ response = requests.get(target)
 response.raise_for_status()
 movie_page = response.text
 soup = BeautifulSoup(movie_page, 'html.parser')
+movie_titles = soup.find_all('img')
+for title in movie_titles:
+    print(title['alt'])
