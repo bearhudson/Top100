ee6e1a556eb6fb7cbda81b336e777ff8461bc5e7
Brian Hudson <reallybrianhudson@gmail.com>
2022-01-14 20:35:47 -0500

    Working better

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ response = requests.get(target)
 response.raise_for_status()
 movie_page = response.text
 soup = BeautifulSoup(movie_page, 'html.parser')
-movie_titles = soup.find_all('img')
+movie_titles = soup.find_all('img', class_='jsx-952983560 loading')
 for title in movie_titles:
     print(title['alt'])
